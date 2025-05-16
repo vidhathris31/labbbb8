10335854a1e3a2306afdd0238e5ec55602964bfc
Vidhathri-S <vidhathris31@gmail.com>
2025-05-16 05:48:34 +0530

    first commit

---
+++ labbbb8/src/main/java/com/vidhathris31/App.java
@@ -0,0 +1,13 @@
+package com.vidhathris31;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
